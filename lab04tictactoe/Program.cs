37c3db605d7cf15f22d96ecc04b5aa37e8f7df0c
shalom <shalom.belaineh@gmail.com>
2019-01-11 11:30:04 -0800

    set up program.cs

---
+++ lab04tictactoe/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lab04tictactoe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
