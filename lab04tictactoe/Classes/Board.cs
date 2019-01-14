1292928eb279698bbbb15e2625815bf41d570d0c
shalom <shalom.belaineh@gmail.com>
2019-01-13 22:17:27 -0800

    worked on board.class

--- lab04tictactoe/Classes/Board.cs
+++ lab04tictactoe/Classes/Board.cs
@@ -19,9 +19,16 @@ namespace Lab04_TicTacToe.Classes
 
         public void DisplayBoard()
         {
-
             //TODO: Output the board to the console
-
+            for (int i = 0; i < 3; i++)
+            {
+                for(int j = 0; j < 3; j++)
+                {
+                    Console.Write($"|{GameBoard[i, j]}|");
+                }
+        Console.WriteLine(" ");
+            }
+            
         }
     }
 }
